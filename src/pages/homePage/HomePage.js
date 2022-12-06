809bce740796241a2f06741f0a28d9872f40b213
BrunodevBandeira <brunodev29ab@gmail.com>
2022-12-06 18:10:50 -0300

    Project WIP

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -13,14 +13,112 @@ const HomePage = () => {
 
 const [ mudar, setMudar ] = React.useState("");
 
-    return( 
-        <section id="Homepage"> 
-        <Head title="Home Page" description="Home Page"/>
-                {/* <p> {String(authenticated)} </p> */}
-            <div className="gallery-menu">
+    const showBox = () => {
+        const boxes = document.querySelectorAll('.box');
 
+        function checkBoxes() {
+            const triggerBottom = window.innerHeight / 5 * 4;
  
+            boxes.forEach(box => {
+                const boxTop = box.getBoundingClientRect().top
+                if(boxTop < triggerBottom) {
+                    box.classList.add('show');
+                } else {
+                    box.classList.remove('show');
+                }
+            })
+        };
 
+        setMudar(checkBoxes());
+        window.addEventListener('scroll', checkBoxes)
+        console.log("bbbbb");
+    };
+
+    React.useEffect(() => {
+        showBox();
+    }, [mudar]);
+
+    return( 
+        <section id="Homepage" onWheel={showBox}> 
+            <Head title="Home Page" description="Home Page"/>
+                {/* <p> {String(authenticated)} </p> */}
+            <div className="gallery-menu"  >
+                {/* <h1> Scroll Animation </h1> */}
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem01"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem02"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem03"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem04"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem05"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem06"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem07"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem08"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem09"/>
+                </div>
+                
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem10"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem11"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem12"/>
+                </div>
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem13"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem14"/>
+                </div>
+
+                <div className="box">
+                    {/* <h2>Content</h2> */}
+                    <img className="imagem15"/>
+                </div>
+ 
             </div>
         </section>
     );
--- src/pages/homePage/HomePageCard.js
+++ src/pages/homePage/HomePageCard.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import "./HomePageCard.css";
+// import React from 'react'
+// import "./HomePageCard.css";
 
-const HomePageCard = ({ children }) => {
-  return (
-    <div id="HomePageCard"> {children} </div>
-  );
-};
+// const HomePageCard = ({ texto }) => {
 
-export default HomePageCard;
+//   const [ mudar, setMudar ] = React.useState([
+ 
+//       { 
+//         id:1, 
+//         photo: "../../assets/leo.jpg",
+//         name: "LEO...",
+//         quantidade: 1
+//       },
+//       { 
+//         id:2, 
+//         photo: "../../assets/marco.jpg",
+//         name: "===> MARCO...",
+//         quantidade: 2
+//       },  
+//   ]);
+
+//   function opa() {
+//     setMudar([...mudar])
+//   }
+
+ 
+
+//   return (
+//     <>
+//       <div id="HomePageCard"> 
+//           <p> { texto } </p> 
+//           <ul>
+//           { 
+//           mudar.map(repo => ( 
+//               <li key={repo.id}> {repo.id} </li>,
+//               <img  key={repo.id} src={repo.photo} alt={repo.name} />
+//               ))
+//           }
+//           </ul>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default HomePageCard;
+
